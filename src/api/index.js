6c1d7a96bd601fde00a0d2b21b50380d5360cebf
guilhercs <guilherme.carvalhos@outlook.com>
2025-12-24 15:17:39 -0300

    refactor: add interceptor

--- src/api/index.js
+++ src/api/index.js
@@ -3,3 +3,20 @@ import axios from "axios";
 export const http = axios.create({
   baseURL: "http://localhost:3000",
 });
+
+const onSuccess = (config) => {
+  const token = localStorage.getItem("access_token");
+
+  if (token) {
+    config.headers = {
+      Authorization: `Bearer ${JSON.parse(token)}`,
+    };
+  }
+  return config;
+};
+
+const onError = (err) => {
+  return err;
+};
+
+http.interceptors.request.use(onSuccess, onError);
